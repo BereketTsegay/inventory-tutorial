522e0734e8550737bca4fe6b47630828ba73bedf
bereket tsegay <eketei23@yahoo.com>
2026-08-08 19:36:57 +0400

    adding he brand controller and request component

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -10,11 +10,47 @@ class BrandController extends Controller
 
     public function AllBrand()
     {
-        return view('pages.brand.all');
+        $headers = Schema::getColumnListing((new Brand)->getTable());
+
+        $brands = Brand::all()->paginate(10); // Adjust the number of items per page as needed
+
+        return view('pages.brand.all', compact('brands', 'headers'));
     }
 
     public function AddBrand()
     {
         return view('pages.brand.add');
+    }
+
+    public function StoreBrand(BrandRequest $request)
+    {
+        
+        $brand = $request->id ? Brand::find($request->id) : new \App\Models\Brand();
+        $brand->brand_name = $request->input('name');
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $imageName = time() . '_' . $image->getClientOriginalName();
+            $image->move(public_path('uploads/brands'), $imageName);
+            $brand->brand_image = 'uploads/brands/' . $imageName;
+        }
+
+        $brand->save();
+
+        return redirect()->route('all.brand')->with('success', 'Brand ' . $brand->brand_name . ' saved successfully.');
+    }
+
+    public function DeleteBrand($id)
+    {
+        $brand = Brand::findOrFail($id);
+        
+        //delete the brand image file if it exists
+        if ($brand->brand_image && file_exists(public_path($brand->brand_image))) {
+            unlink(public_path($brand->brand_image));
+        }
+
+        $brand->delete();
+
+        return redirect()->route('all.brand')->with('success', 'Brand deleted successfully.');
     }
 }
---
+++ app/Http/Requests/BrandRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class BrandRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+            'name' => ['required',  Rule::unique('brands', 'brand_name')->ignore($this->brand)],
+            'image' => 'nullable|image|mimes:jpg,jpeg,png|max:2048',
+        ];
+    }
+}
